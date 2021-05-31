1abcac682294c3fcf1d1b148bde6aa451a468292
KsaweryM <57969843+KsaweryM@users.noreply.github.com>
2021-05-31 21:27:08 +0200

    Delete src/test directory

--- src/test/integer-buffer-test.h
+++
@@ -1,8 +0,0 @@
-/**
- * The test verifies the correctness of the implementation of integer-buffer.
- * There are two threads and first one sends integers into second one by integer-buffer.
- * The test is passed if the input data is the same as the output data.
- * Otherwise program will be aborted. 
- */
-
-void integer_buffer_test();
--- src/test/watchdog-test.h
+++
@@ -1,2 +0,0 @@
-void watchdog_test();
-
