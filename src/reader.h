bc056f9a790d08461aaa88afc38595d3ebfbbedf
KsaweryM <57969843+KsaweryM@users.noreply.github.com>
2021-06-01 20:02:05 +0200

    Update reader.h

--- src/reader.h
+++ src/reader.h
@@ -8,13 +8,7 @@
 /*
 * Reader is a thread that reads from /proc/stat file information about processors and sends it to analyzer_buffer.
 * At the beginning reader counts how many processors there are in the computer. Let's assume that there are N processors. 
-<<<<<<< HEAD
-* Reader thread in the loop opens proc/stat file, avoids the first line, reads next N lines and send them to output buffer, close file. 
-* 
 * Reader thread also informs watchdog thread about itself activity and sends logs info to logger thread. 
-=======
-* Reader in the loop opens proc/stat file, avoids the first line, reads next N lines and sends them to output buffer, close file. 
->>>>>>> 875bc0abbd33f771f3455835fc1c3719ae827ca8
 */
 typedef struct reader reader;
 
